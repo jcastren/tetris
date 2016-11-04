d547adb01714cb9e17095039441ba9954d5ff44a
jcastren <joonas_castren@yahoo.com>
2016-11-04 09:27:38 +0200

    Changed package name

---
+++ tetris/src/main/java/fi/joonas/tetris/TetrisUtil.java
@@ -0,0 +1,12 @@
+package fi.joonas.tetris;
+
+public abstract class TetrisUtil {
+	
+	/**
+	 * Convenience method from System.out.println
+	 * @param str
+	 */
+	public static void out(String str) {
+		System.out.println(str);
+	}
+}
